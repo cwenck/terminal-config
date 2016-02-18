43daffdf5881ac9fe0e8427182d3c4756dd4d66f
Connor Wenck <cwenck@me.com>
2016-02-17 21:22:09 -0500

    added alias to hide and show desktop icons

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -44,8 +44,8 @@ alias hideFiles='defaults write com.apple.finder AppleShowAllFiles -bool false; 
 alias showPathBar='defaults write com.apple.finder ShowPathbar -bool true; killall Finder'
 alias hidePathBar='defaults write com.apple.finder ShowPathbar -bool false; killall Finder'
 
-alias hideDesktop='defaults write com.apple.finder CreateDesktop -bool false'
-alias showDesktop='defaults write com.apple.finder CreateDesktop -bool true'
+alias hideDesktop='defaults write com.apple.finder CreateDesktop -bool false; killall Finder'
+alias showDesktop='defaults write com.apple.finder CreateDesktop -bool true; killall Finder'
 
 #UMD SSH
 alias grace='ssh cwenck@linux.grace.umd.edu'
