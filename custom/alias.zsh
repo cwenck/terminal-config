db24af9c6e5357264f2fb56b2b7e3ac1019cf154
Connor Wenck <cwenck@me.com>
2016-04-19 16:13:05 -0400

    updated port for vzhost

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -1,3 +1,6 @@
+#Tmp SSH Alias
+alias tmp='ssh -p 49759 root@aceslab.umd.edu'
+
 #Common Directory Aliases
 alias dsk='cd ~/Desktop/'
 alias dropbox='cd ~/Dropbox/'
@@ -16,7 +19,7 @@ alias cyber='cd ~/.cyber'
 alias subl-snippet='cd "/Users/connor/Library/Application Support/Sublime Text 3/Packages/User"'
 
 #Honeypot Aliases
-alias vzhost='ssh -p 60038 hp@aceslab.umd.edu'
+alias vzhost='ssh -p 49759 hp@aceslab.umd.edu'
 
 #Programing Directories
 alias code='cd ~/Dropbox/Programming/'
