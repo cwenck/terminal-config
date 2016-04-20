71fb05282cbdf6186414cf1bc357640960af7648
Connor Wenck <cwenck@me.com>
2016-04-20 10:26:34 -0400

    updated paths

--- custom/paths.zsh
+++ custom/paths.zsh
@@ -1,5 +1,5 @@
 PATH=/usr/local/sbin:$PATH
-PATH=~/.bin:$PATH
+PATH=~/.usr/bin:$PATH
 PATH=$PATH:/Applications/Matlab.app/bin
 PATH=$PATH:/Applications/Sublime\ Text.app/Contents/SharedSupport/bin
 
