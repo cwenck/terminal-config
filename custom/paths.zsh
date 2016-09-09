24e1644f458a72304b44bb338d9595ae4a2a5e97
Connor Wenck <connorwenck@gmail.com>
2016-09-09 19:40:56 -0400

    added sublime icons and updated zsh configuration

--- custom/paths.zsh
+++ custom/paths.zsh
@@ -3,6 +3,7 @@ PATH=~/.usr/bin:$PATH
 PATH=$PATH:/Applications/Matlab.app/bin
 PATH=$PATH:/Applications/Sublime\ Text.app/Contents/SharedSupport/bin
 PATH=$PATH:/usr/local/opt/go/libexec/bin:$HOME/.go/bin/
+PATH=$HOME/bin:/usr/local/bin:$PATH
 
 PATH=.:$PATH
 export PATH
